e2b41347d3277c328bf8d8d280798928708e174e
Adam St. John <astjohn@gmail.com>
2016-07-23 22:50:52 -0400

    More FCM fields.

--- ccs/message.go
+++ ccs/message.go
@@ -4,28 +4,33 @@ package ccs
 // If ID field is not set, it will be generated automatically using crypto/rand.
 // Google recommends Data field to be strings key/value pairs and keys cannot be
 // reserved words described in GCM server documentation.
-// https://developer.android.com/google/gcm/ccs.html#format
+// https://firebase.google.com/docs/cloud-messaging/xmpp-server-ref
 type OutMsg struct {
 	To                       string            `json:"to"`
+	Condition                string            `json:"condition,omitempty"`
 	ID                       string            `json:"message_id"`
+	CollapseKey              string            `json:"collapse_key,omitempty"`
+	Priority                 string            `json:"priority,omitempty"`
+	ContentAvail             bool              `json:"content_available,omitempty"`
+	DelayWhileIdle           bool              `json:"delay_while_idle,omitempty"`
+	TimeToLive               int               `json:"time_to_live,omitempty"`               //default:2419200 (in seconds = 4 weeks)
+	DeliveryReceiptRequested bool              `json:"delivery_receipt_requested,omitempty"` //default:false
+	DryRun                   bool              `json:"dry_run,omitempty"`                    //default:false
 	Data                     map[string]string `json:"data,omitempty"`
 	Notification             map[string]string `json:"notification,omitempty"`
-	MessageType              string            `json:"message_type,omitempty"`
-	CollapseKey              string            `json:"collapse_key,omitempty"`
-	TimeToLive               int               `json:"time_to_live,omitempty"`               //default:2419200 (in seconds = 4 weeks)
-	DelayWhileIdle           bool              `json:"delay_while_idle,omitempty"`           //default:false
-	DeliveryReceiptRequested bool              `json:"delivery_receipt_requested,omitempty"` //default:false
 }
 
 // InMsg is an incoming GCM CCS message.
 type InMsg struct {
-	From         string            `json:"from"`
-	ID           string            `json:"message_id"`
-	Category     string            `json:"category"`
-	Notification map[string]string `json:"notification"`
-	Data         map[string]string `json:"data"`
-	MessageType  string            `json:"message_type"`
-	ControlType  string            `json:"control_type"`
-	Err          string            `json:"error"`
-	ErrDesc      string            `json:"error_description"`
+	From           string            `json:"from"`
+	ID             string            `json:"message_id"`
+	Category       string            `json:"category"`
+	Priority       string            `json:"priority"`
+	Data           map[string]string `json:"data"`
+	Notification   map[string]string `json:"notification"`
+	MessageType    string            `json:"message_type"`
+	RegistrationID string            `json:"registration_id"`
+	ControlType    string            `json:"control_type"`
+	Err            string            `json:"error"`
+	ErrDesc        string            `json:"error_description"`
 }
